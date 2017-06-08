08b7909af1b0a64b617dd03b4a790d98be839d54
Philip Weaver <simplygreatwork@gmail.com>
2017-06-08 03:25:11 +0000

    Adding an example which illustrates learning authentication in addition to learning authorization.

---
+++ examples/learning-authentication/example.js
@@ -0,0 +1,146 @@
+
+var godsend = require('godsend');
+var basic = require('godsend-basics');
+var Class = godsend.Class; 
+var uuid = require('uuid');
+
+Example = Class.extend({
+	
+	initialize: function() {
+		
+		new basic.Server({
+			learn : false
+		}).start(function() {
+			new basic.Authorizer().connect(function() {
+				new Agent().start();
+				new Sender().start();
+				console.log('The example has started.');
+			}.bind(this));
+		}.bind(this));
+	}
+});
+
+Agent = Class.extend({
+	
+	start: function() {
+		
+		var connection = godsend.connect({
+			address: basic.Utility.local(),
+			credentials: {
+				username: basic.Credentials.get('agent').username,
+				passphrase: basic.Credentials.get('agent').passphrase,
+			}
+		});
+		
+		connection.mount({
+			id: 'transform-object',
+			on: function(request) {
+				request.accept({
+					action: 'transform-object'
+				});
+			}.bind(this),
+			run: function(stream) {
+				var object = stream.object;
+				object.date = new Date();
+				stream.push(object);
+				stream.next();
+			}.bind(this)
+		});
+		
+		connection.mount({
+			id: 'presence-online',
+			on: function(request) {
+				request.accept({
+					topic : 'presence',
+					action: 'online'
+				});
+			}.bind(this),
+			run: function(stream) {
+				console.log('The agent was notified that "' + stream.object.username + '" has come online.');
+				stream.push({
+					acknowledge : true
+				});
+				stream.next();
+			}.bind(this)
+		});
+		
+		connection.mount({
+			id: 'presence-offline',
+			on: function(request) {
+				request.accept({
+					topic : 'presence',
+					action: 'offline'
+				});
+			}.bind(this),
+			run: function(stream) {
+				console.log('The agent was notified that "' + stream.object.username + '" has gone offline.');
+				stream.push({
+					acknowledge : true
+				});
+				stream.next();
+			}.bind(this)
+		});
+	}
+});
+
+Sender = Class.extend({
+	
+	start: function() {
+		
+		var connection = godsend.connect({
+			address: basic.Utility.local(),
+			credentials: {
+				username: basic.Credentials.get('sender').username,
+				passphrase: basic.Credentials.get('sender').passphrase,
+			}
+		});
+		
+		var sequence = basic.Sequence.start(
+			
+			function() {
+				
+				connection.send({
+					pattern: {
+						action: 'transform-object'
+					},
+					data : {
+						type : 'object'
+					},
+					receive: function(result) {
+						console.log('result: ' + JSON.stringify(result, null, 2));
+						sequence.next();
+					}.bind(this)
+				});
+				
+			}.bind(this),
+			
+			function() {
+				
+				connection.send({
+					pattern: {
+						topic : 'authentication',
+						action : 'sign-out'
+					},
+					data : {},
+					receive: function(result) {
+						console.log('result: ' + JSON.stringify(result, null, 2));
+						sequence.next();
+					}.bind(this)
+				});
+
+			}.bind(this),
+			
+			function() {
+				
+				setTimeout(function() {
+					console.log('The example has finished.');
+					process.exit(0);
+				}.bind(this), 500);
+				
+			}.bind(this)
+			
+		);
+	}
+});
+
+new Example();
